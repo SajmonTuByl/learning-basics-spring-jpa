e1f6e1ab8e70ce3f66f4ab5cab7e998a51b06fad
unknown <nomjas666@gmail.com>
2022-09-06 00:06:14 +0200

    First commit

---
+++ src/main/java/com/example/learningbasicsspringjpa/LearningBasicsSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningbasicsspringjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningBasicsSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningBasicsSpringJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learningbasicsspringjpa/LearningBasicsSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningbasicsspringjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningBasicsSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
